e96bd84c50cb4ff9d3ebbab27827fdd67914810f
joshupadhyay <joshupad@gmail.com>
2020-03-12 15:44:38 -0500

    changed page name, test commit

---
+++ lib/models/dreamentry.dart
@@ -0,0 +1,18 @@
+
+
+class DreamEntry {
+  static const String PassionCooking = 'cooking';
+  static const String PassionHiking = 'hiking';
+  static const String PassionTraveling = 'traveling';
+  String firstName = '';
+  String lastName = '';
+  Map passions = {
+    PassionCooking: false,
+    PassionHiking: false,
+    PassionTraveling: false
+  };
+
+  save() {
+    print('saving user using a web service');
+  }
+}

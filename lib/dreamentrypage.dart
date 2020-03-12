4ef0d7b4964708fb24bb7c305a1f11ffadf1bae2
joshupadhyay <joshupad@gmail.com>
2020-03-12 16:07:44 -0500

    changed to home: DreamEntryPage() in 'main.dart' to display my progress. temp fix for now.

---
+++ lib/dreamentrypage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'models/dreamentry.dart';
+
+class DreamEntryPage extends StatefulWidget{
+  @override
+  _DreamEntryPageState createState() => _DreamEntryPageState();
+
+}
+
+class _DreamEntryPageState extends State<DreamEntryPage>{
+  final _dreamentry = DreamEntry();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("New Dream Entry")),
+      body: Container(
+        padding:
+          const EdgeInsets.symmetric(vertical: 40),
+        child: Builder(
+          builder: (context) => Form(//contains all logical stuff
+            key: _formKey, //will be using formkey to get access to form data
+            child: Column(//container for all our form fields
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(),
+                TextFormField(),
+                TextFormField(),
+              ],
+
+            )
+          )
+        )
+      ),
+    );
+  }
+
+
+
+}
--- lib/editdream.dart
+++ lib/editdream.dart
@@ -1,15 +1,3 @@
 import 'package:flutter/material.dart';
 import 'homepage.dart';
 
-
-class App extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-
-
-
-    );
-  }
-
-}

fa26eea0089410ed33e1cc4218da9ab6878aabe3
Federico <fedechung@gmail.com>
2020-03-12 16:59:32 -0500

    homepage set up

---
+++ lib/homepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text("Dream Journal"),
+        ),
+        body: new Column(
+          children: <Widget>[
+            Container(
+
+            )
+
+          ],
+        )
+    );
+  }
+}

480e9290e50592bd040262a430f523df1bb105ed
joshupadhyay <joshupad@gmail.com>
2020-03-15 11:52:56 -0400

    Text and buttons now both show on page, both now separate widgets.

--- lib/dreamentrypage.dart
+++ lib/dreamentrypage.dart
@@ -2,7 +2,6 @@ import 'package:dreamjournal/EmoteButton.dart';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
 import 'models/dreamentry.dart';
-import 'package:multi_type_list_view/multi_type_list_view.dart';
 
 class DreamEntryPage extends StatefulWidget{
   @override
@@ -10,87 +9,22 @@ class DreamEntryPage extends StatefulWidget{
 
 }
 
-
-
 class _DreamEntryPageState extends State<DreamEntryPage>{
   final _dreamentry = DreamEntry(); //for when we start saving responses to the forms, etc
 
-
-  List<EmoteButton> doInit() {
-    var buttonsList = <EmoteButton>[
-      new EmoteButton(id:1, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),  // just did a couple random colors for proof of concept
-      new EmoteButton(id:2),
-      new EmoteButton(id:3),
-      new EmoteButton(id:4, bg:Colors.orange, color1:Colors.orange, color2:Colors.lightGreenAccent),
-      new EmoteButton(id:5),
-      new EmoteButton(id:6),
-      new EmoteButton(id:7, bg:Colors.orange, color1:Colors.orange, color2:Colors.lightGreenAccent),
-      new EmoteButton(id:8, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),
-      new EmoteButton(id:9),
-      new EmoteButton(id:10),
-      new EmoteButton(id:11, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),
-      new EmoteButton(id:12),
-    ];
-    return buttonsList;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    List<EmoteButton> buttons = doInit(); //current declaration
-  }
-
-  void switchState(EmoteButton b) {
-    setState(() { // to recall build, so it rebuilds
-      b.on = !b.on; // to switch on and off
-      if (b.on) {
-        b.bg = b.color2;  // bg is the actual displayed color, color1 and color2 are just the on/off colors respectively
-      }
-      else {
-        b.bg = b.color1;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text("New Dream Entry"), centerTitle: true,),
-      body: Container(
-        padding:
-          const EdgeInsets.symmetric(vertical: 40),
-        child: Column(
-          children: <Widget>[textFieldsBuilder(context),
-//                GridView.builder(           // OK GUYS, I commented this out because it uses a different kind of builder, but it would actually be really convenient to have multiple types, so either we can implement that (link: https://flutterawesome.com/a-flutter-customer-listview-that-displays-multiple-widget-types/) Or we can find a different solution
-//                  padding: const EdgeInsets.all(18.0),  // padding from edge of screen
-//                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                    crossAxisCount: 3,  // how many across
-//                    childAspectRatio: 1.0,  //increase to make squashed
-//                    crossAxisSpacing: 18.0, // this num and num below should be the same for padding
-//                    mainAxisSpacing: 18.0,
-//                  ),
-//                  itemCount: buttons.length,
-//                  itemBuilder: (context, i) => SizedBox(
-//                    width: 100.0,
-//                    height: 100.0,
-//                    child: RaisedButton(
-//                      padding: const EdgeInsets.all(18.0),
-//                      shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-//                      onPressed: buttons[i].enabled?()=>switchState(buttons[i]):null, // if the button is enabled, call switchState, else do null
-//                      child: Text(
-//                          buttons[i].activeText,
-//                          style: TextStyle(color: Colors.limeAccent,
-//                              fontSize: 40.0)),
-//                      color: buttons[i].bg,
-//                      disabledColor: buttons[i].bg,
-//                    ),
-//                  ),
-//                ),
-          ],
-        )
-      ),
-    );
+      body: Column(
+        children: <Widget>[textFieldsBuilder(context),
+        new Expanded(child: buttonEmotionBuilder())
+          //TODO add a border, make the scrolling look nicer
+        ],
+
+      )
+      );
+
   }
 }
 
@@ -121,49 +55,74 @@ Widget textFieldsBuilder (BuildContext context) {
 
 
 
+/*
+* Not sure if we can do this more efficiently, but https://flutter.dev/docs/development/ui/interactive
+* shows we need to make a class that extends stateful widget, then have that extend a state
+* */
+class buttonEmotionBuilder extends StatefulWidget{
+  _ButtonEmotionState createState() => _ButtonEmotionState();
+}
 
-//Trying to separate the widgets for nicer code. We should be able to
+class _ButtonEmotionState extends State<buttonEmotionBuilder>{
 
-//Widget buttonEmotionBuilder (){
-//
-//  var buttonsList = <EmoteButton>[
-//    new EmoteButton(id:1, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),  // just did a couple random colors for proof of concept
-//    new EmoteButton(id:2),
-//    new EmoteButton(id:3),
-//    new EmoteButton(id:4, bg:Colors.orange, color1:Colors.orange, color2:Colors.lightGreenAccent),
-//    new EmoteButton(id:5),
-//    new EmoteButton(id:6),
-//    new EmoteButton(id:7, bg:Colors.orange, color1:Colors.orange, color2:Colors.lightGreenAccent),
-//    new EmoteButton(id:8, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),
-//    new EmoteButton(id:9),
-//    new EmoteButton(id:10),
-//    new EmoteButton(id:11, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),
-//    new EmoteButton(id:12),
-//  ];
-//
-//  GridView.builder(           // OK GUYS, I commented this out because it uses a different kind of builder, but it would actually be really convenient to have multiple types, so either we can implement that (link: https://flutterawesome.com/a-flutter-customer-listview-that-displays-multiple-widget-types/) Or we can find a different solution
-//    padding: const EdgeInsets.all(18.0),  // padding from edge of screen
-//    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//      crossAxisCount: 3,  // how many across
-//      childAspectRatio: 1.0,  //increase to make squashed
-//      crossAxisSpacing: 18.0, // this num and num below should be the same for padding
-//      mainAxisSpacing: 18.0,
-//    ),
-//    itemCount: buttonsList.length,
-//    itemBuilder: (context, i) => SizedBox(
-//      width: 100.0,
-//      height: 100.0,
-//      child: RaisedButton(
-//        padding: const EdgeInsets.all(18.0),
-//        shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-//        onPressed: buttonsList[i].enabled?()=>switchState(buttonsList[i]):null, // if the button is enabled, call switchState, else do null
-//        child: Text(
-//            buttonsList[i].activeText,
-//            style: TextStyle(color: Colors.limeAccent,
-//                fontSize: 40.0)),
-//        color: buttonsList[i].bg,
-//        disabledColor: buttonsList[i].bg,
-//      ),
-//    ),
-//  );
-//}
+  var buttonsList = <EmoteButton>[
+    new EmoteButton(id:1, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),  // just did a couple random colors for proof of concept
+    new EmoteButton(id:2),
+    new EmoteButton(id:3),
+    new EmoteButton(id:4, bg:Colors.orange, color1:Colors.orange, color2:Colors.lightGreenAccent),
+    new EmoteButton(id:5),
+    new EmoteButton(id:6),
+    new EmoteButton(id:7, bg:Colors.orange, color1:Colors.orange, color2:Colors.lightGreenAccent),
+    new EmoteButton(id:8, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),
+    new EmoteButton(id:9),
+    new EmoteButton(id:10),
+    new EmoteButton(id:11, bg:Colors.blue, color1: Colors.blue, color2: Colors.deepOrange),
+    new EmoteButton(id:12),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void switchState(EmoteButton b) {
+    setState(() { // to recall build, so it rebuilds
+      b.on = !b.on; // to switch on and off
+      if (b.on) {
+        b.bg = b.color2;  // bg is the actual displayed color, color1 and color2 are just the on/off colors respectively
+      }
+      else {
+        b.bg = b.color1;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(18.0),  // padding from edge of screen
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,  // how many across
+        childAspectRatio: 1.0,  //increase to make squashed
+        crossAxisSpacing: 18.0, // this num and num below should be the same for padding
+        mainAxisSpacing: 18.0,
+      ),
+      itemCount: buttonsList.length,
+      itemBuilder: (context, i) => SizedBox(
+        width: 100.0,
+        height: 100.0,
+        child: RaisedButton(
+          padding: const EdgeInsets.all(18.0),
+          shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+          onPressed: buttonsList[i].enabled?()=>switchState(buttonsList[i]):null, // if the button is enabled, call switchState, else do null
+          child: Text(
+              buttonsList[i].activeText,
+              style: TextStyle(color: Colors.limeAccent,
+                  fontSize: 40.0)),
+          color: buttonsList[i].bg,
+          disabledColor: buttonsList[i].bg,
+        ),
+      ),
+    );
+  }
+}

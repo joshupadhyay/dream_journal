4fe6d429937a74d747a5ccaf49d3dbc740a720bd
Federico <fedechung@gmail.com>
2020-03-16 21:15:34 -0500

    add the button icon

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,20 +5,57 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+
+
+
 class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
         body: Center(
-      child: FlatButton.icon(
-          onPressed: null,
-          label: Text(""),
-          icon: Icon(
-            Icons.add_box,
-            color: Colors.deepPurple,
-            size: 200.0,
-          )),
-    ));
+          child:Column(
+            children: <Widget>[
+              Text(
+                '$_counter',
+                style: TextStyle(fontFamily: "Pacifico",
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                ),
+
+              ),
+              FlatButton.icon(
+                onPressed: _incrementCounter,
+                label: Text("Add Dream",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontFamily: "Pacifico",
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold
+                    )),
+                icon: Icon(
+                  Icons.add_circle,
+                  color: Colors.pinkAccent,
+                  size: 250.0,
+                ),
+
+              )
+            ],
+
+
+          )
+
+    )
+    );
   }
 }

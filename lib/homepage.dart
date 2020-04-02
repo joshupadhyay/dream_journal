3ba5aa01482756ca107f5cd185f6cb50ae54b4e2
Federico <fedechung@gmail.com>
2020-04-02 17:09:47 -0500

    homepage decorations and additions

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'adddreampage.dart';
 
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-
-
 
 
 class _HomePageState extends State<HomePage> {
@@ -15,21 +14,41 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        body: Center(
-          child:Column(
-            children: <Widget>[
-               newDreamEntryButton(context)
-            ],
+      backgroundColor: Image.asset('').color,
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 10,
+              right: 0,
+              top: 200,
+              child: newDreamEntryButton(context),
+            ),
 
+            Positioned(
+                left: 120,
+                top: 350,
+                child: Text(
+                  "new dream",
+                  style: TextStyle(
+                      fontFamily: "Permanent",
+                      fontSize: 30.0,
+                      color: Colors.white
+                  ),
+                )
+            ),
 
-          )
-
-    )
+          ],
+        ),
+      ),
     );
   }
 }
 
-Widget newDreamEntryButton(BuildContext context){
+//newDreamEntryButton(context),
+
+
+Widget newDreamEntryButton(BuildContext context) {
   return FlatButton.icon(onPressed: () {
     Navigator.push(
       context,
@@ -39,9 +58,28 @@ Widget newDreamEntryButton(BuildContext context){
     Icons.add_circle,
     color: Colors.pinkAccent,
     size: 250.0,),
-      label: Text("Add Dream",
-          textAlign: TextAlign.left,
-          style: TextStyle(fontFamily: "Pacifico",
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold)));
+      label: Text("")
+  );
 }
+
+
+Widget newDreamViewerButton(BuildContext context) {
+  return FlatButton.icon(onPressed: () {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => new RecentDreams()),
+    );
+  }, icon: Icon(
+    Icons.add_circle,
+    color: Colors.pinkAccent,
+    size: 250.0,),
+      label: Text("")
+  );
+}
+
+
+//Text("",
+//textAlign: TextAlign.left,
+//style: TextStyle(fontFamily: "Pacifico",
+//fontSize: 20.0,
+//fontWeight: FontWeight.bold)));

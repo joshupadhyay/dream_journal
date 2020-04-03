b48da20ed3679e26c7f9e55442b448b632300d7b
Federico <fedechung@gmail.com>
2020-04-02 23:25:58 -0500

    UI
    changed UI design

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'adddreampage.dart';
 import 'recentdreams.dart';
 
@@ -17,10 +18,11 @@ class _HomePageState extends State<HomePage> {
     return Stack(
       children: <Widget>[
         Image.asset(
-          "assets/images/dreams.jpeg",
+          "assets/images/dreams.jpg",
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           fit: BoxFit.cover,
+
         ),
         Scaffold(
           backgroundColor: Colors.transparent,
@@ -28,23 +30,76 @@ class _HomePageState extends State<HomePage> {
             child: Stack(
               children: <Widget>[
                 Positioned(
-                  left: 10,
+                  left: 0,
                   right: 0,
-                  top: 150,
+                  top: 25,
+                  child: Icon(
+                    FontAwesomeIcons.bookOpen,
+                    color: Colors.black54,
+                    size: 120.0,
+                  ),
+                ),
+                Positioned(
+                  left: -75,
+                  right: 0,
+                  top: 35,
+                  child: Text(
+                    "D",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: "Pacifico",
+                      fontSize: 55.0,
+                      color: Colors.green[200],
+
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 70,
+                  right: 0,
+                  top: 35,
+                  child: Text(
+                    "J",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: "Pacifico",
+                      fontSize: 55.0,
+                      color: Colors.blueGrey[200],
+
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  left: 20,
+                  right: 10,
+                  top: 300,
                   child: newDreamEntryButton(context),
                 ),
 
                 Positioned(
-                    left: 120,
-                    top: 400,
+                    left: 140,
+                    top: 510,
                     child: Text(
                       "new dream",
                       style: TextStyle(
                           fontFamily: "Permanent",
-                          fontSize: 30.0,
-                          color: Colors.white
+                          fontSize: 25.0,
+                          color: Colors.black87
                       ),
                     )
+                ),
+                Positioned(
+                  left: 300,
+                  right: 0,
+                  top: 720,
+                  child: newDreamViewerButton(context),
+                ),
+                Positioned(
+                  left: -300,
+                  right: 0,
+                  top: 720,
+                  child: newDreamAnalysisTab(context),
                 ),
 
               ],
@@ -67,9 +122,11 @@ Widget newDreamEntryButton(BuildContext context) {
       new MaterialPageRoute(builder: (context) => new DreamEntryPage()),
     );
   }, icon: Icon(
-    Icons.add_circle,
-    color: Colors.pinkAccent,
-    size: 250.0,),
+    FontAwesomeIcons.plus,
+    color: Colors.black87,
+    size: 220.0,
+
+  ),
       label: Text("")
   );
 }
@@ -82,9 +139,24 @@ Widget newDreamViewerButton(BuildContext context) {
       new MaterialPageRoute(builder: (context) => new RecentDreams()),
     );
   }, icon: Icon(
-    Icons.storage,
-    color: Colors.pinkAccent,
-    size: 250.0,),
+    FontAwesomeIcons.book,
+    color: Colors.blueGrey[200],
+    size: 60.0,),
+      label: Text("")
+  );
+}
+
+
+Widget newDreamAnalysisTab(BuildContext context) {
+  return FlatButton.icon(onPressed: () {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(builder: (context) => new RecentDreams()),
+    );
+  }, icon: Icon(
+    FontAwesomeIcons.chartLine,
+    color: Colors.green[200],
+    size: 60.0,),
       label: Text("")
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Dream Journal UI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blueGrey,
+        accentColor: Color(0xFFB39DDB),
+        scaffoldBackgroundColor: Colors.grey[600],
+
       ),
       home: HomePage(),//change this to see how your page is... def some way to show other pages without home!
     );

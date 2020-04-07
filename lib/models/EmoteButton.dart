1eb002620d0674f163c8c59fc9058d4eb37abc9d
Aidan Alls <aalls@macalester.edu>
2020-04-07 14:46:48 -0700

    Added some comments to emote button to make functionality clearer

--- lib/models/EmoteButton.dart
+++ lib/models/EmoteButton.dart
@@ -6,8 +6,8 @@ class EmoteButton{
   Color bg; // when initialized, bg and color1 have to be the same
   Color color1;
   Color color2;
-  bool enabled;
-  bool on;
+  bool enabled; // whether or not can be pressed
+  bool on;  // whether or not is currently pressed
   AssetImage emoji;
 
   EmoteButton({this.id, this.emoji, this.activeText = "", this.bg = Colors.grey, this.color1 = Colors.grey, this.color2 = Colors.yellowAccent, this.enabled = true, this.on = false});
